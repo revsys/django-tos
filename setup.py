42d43e3b86e72a642f2e92f6a5aaa2ab51a2d4f4
pydanny <pydanny@gmail.com>
2010-06-18 11:24:33 -0500

    More boilerplate

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+from tos import VERSION
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.txt'))
+readme = f.read()
+f.close()
+
+setup(
+    name='django-tos',
+    version=".".join(map(str, VERSION)),
+    description='django-tos is a reusable Django application for setting Terms of Service.',
+    long_description=readme,
+    author='Daniel Greenfeld',
+    author_email='pydanny@gmail.com',
+    url='http://github.com/revsys/django-tos/tree/master',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
