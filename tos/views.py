8f674e255aaccdbbf044caf82dc7046c056731fe
Frank Wiles <frank@revsys.com>
2010-06-21 12:09:16 -0500

    Minor fixes to views and fixture

--- tos/views.py
+++ tos/views.py
@@ -115,7 +115,7 @@ def login(request, template_name='registration/login.html',
 
     return render_to_response(template_name, {
         'form': form,
-        redirect_field_name: redirect_to,
+        'redirect_field_name': redirect_to,
         'site': current_site,
         'site_name': current_site.name,
     }, context_instance=RequestContext(request))
