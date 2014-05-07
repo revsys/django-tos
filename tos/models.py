f2fbb92f4294be3d101f3640e60e77b682da7cce
Frank Wiles <frank@revsys.com>
2014-05-07 09:05:06 -0500

    Merge fixes

--- tos/models.py
+++ tos/models.py
@@ -1,9 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
-from django.utils.translation import ugettext_lazy as _
 
 
 class NoActiveTermsOfService(ValidationError):
--- tos/urls.py
+++ tos/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url, patterns
 from tos.views import check_tos, TosView
 
 urlpatterns = patterns('',
-        # Terms of Service conform 
+        # Terms of Service conform
         url(r'^confirm/$', check_tos, name='tos_check_tos'),
-        
-        # Terms of service simple display 
+
+        # Terms of service simple display
         url(r'^$', TosView.as_view(), name='tos'),
     )
