eb863621ffeca9ec7fe79c74cc60172cbac20ae1
blag <blag@users.noreply.github.com>
2021-12-20 02:01:25 -0800

    Replace django.conf.urls.url with django.urls.re_path

--- tos/tests/test_urls.py
+++ tos/tests/test_urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.views.generic import TemplateView
 
 from tos import views
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
+    re_path(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
 
-    url(r'^login/$', views.login, {}, 'login'),
-    url(r'^tos/', include('tos.urls')),
+    re_path(r'^login/$', views.login, {}, 'login'),
+    re_path(r'^tos/', include('tos.urls')),
 ]
