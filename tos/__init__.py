4635e4495f508018bc1b156b63cffd97232b0ed7
Jeff Triplett <jeff.triplett@gmail.com>
2026-08-01 21:53:48 -0500

    Bump version 1.2.0 -> 1.2.1

--- tos/__init__.py
+++ tos/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 
 # Kept for backwards compatibility with anything importing the tuple form.
 VERSION = tuple(int(part) for part in __version__.split("."))
